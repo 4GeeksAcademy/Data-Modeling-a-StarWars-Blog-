c428742c5d198e87f2b24614c71c88d83463e8c1
mariaperez15 <123765659+mariaperez15@users.noreply.github.com>
2023-05-26 20:55:56 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -12,57 +12,44 @@ class User(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    fullname = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
     email= Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
+    subscription_date = Column(String(50), nullable=False)
 
-class Planets (Base):
-    __tablename__ = 'planets'
+class Planets(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nulllable=False)
     climate = Column(String(20))
-    diametro = Column(Integer(20))
+    diametro = Column(Integer)
     terreno = Column (String(20))
     url = Column (String(20))
 
-class Character (Base):
+class Character(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nulllable=False)
-    films = Column(String(20))
-    gener = Column(Integer(20))
+    gender = Column(Integer(20))
     eye_colour = Column (String(20))
-    spwecies = Column (String(20))
+    species = Column (String(20))
     movies = Column(String(20))
 
-class Characters_Favourites (Base):
-    __tablename__ = 'characters_favourites'
+class Characters_Favorites(Base):
+    __tablename__ = 'characters_favorites'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer(50), nulllable=False)
-    character_id = Column(Integer(20), nulllable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    character_id = Column(Integer, ForeignKey('character.id'))
+    user_id = Column(Integer(50), ForeignKey('user.id'))
+    character_id = Column(Integer(20), ForeignKey('character.id'))
 
-class Planets_Favourites (Base):
+class Planets_Favourites(Base):
     __tablename__ = 'planets_favourites'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer(50), nulllable=False)
-    character_id = Column(Integer(20), nulllable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    planet_id = Column(Integer, ForeignKey('planet.id'))
-    
+    user_id = Column(Integer(50), ForeignKey('user.id'))
+    planet_id = Column(Integer(20), ForeignKey('planet.id'))
     
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
 
     def to_dict(self):
         return {}
